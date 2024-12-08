077aea57c3425d2cddeb8cdce51f5593779b5c77
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-12-08 01:22:50 -0500

    annotated bomb.asm

--- labs/lab2-bomb/bomb.asm
+++ labs/lab2-bomb/bomb.asm
@@ -427,49 +427,53 @@ Disassembly of section .text:
   400fc9:	48 83 c4 18          	add    $0x18,%rsp
   400fcd:	c3                   	retq   
 
-0000000000400fce <func4>:
-  400fce:	48 83 ec 08          	sub    $0x8,%rsp
-  400fd2:	89 d0                	mov    %edx,%eax
-  400fd4:	29 f0                	sub    %esi,%eax
-  400fd6:	89 c1                	mov    %eax,%ecx
-  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx
-  400fdb:	01 c8                	add    %ecx,%eax
-  400fdd:	d1 f8                	sar    %eax
-  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx
-  400fe2:	39 f9                	cmp    %edi,%ecx
-  400fe4:	7e 0c                	jle    400ff2 <func4+0x24>
-  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx
-  400fe9:	e8 e0 ff ff ff       	callq  400fce <func4>
-  400fee:	01 c0                	add    %eax,%eax
-  400ff0:	eb 15                	jmp    401007 <func4+0x39>
-  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax
-  400ff7:	39 f9                	cmp    %edi,%ecx
-  400ff9:	7d 0c                	jge    401007 <func4+0x39>
-  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi
+0000000000400fce <func4>: // rdi is i, rsi is 0, rdx is 14
+  400fce:	48 83 ec 08          	sub    $0x8,%rsp      // add 1 byte to stack
+  400fd2:	89 d0                	mov    %edx,%eax      // move 14 to return 
+  400fd4:	29 f0                	sub    %esi,%eax      // 14 - 0
+  400fd6:	89 c1                	mov    %eax,%ecx      // move 14 to rcx
+  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx     // rcx = rcx >> 31 (rcx = 0)
+  400fdb:	01 c8                	add    %ecx,%eax      // return += rcx (return + 0)
+  400fdd:	d1 f8                	sar    %eax	      // return >> 1 (return = 7)
+  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx   // rcx = 7
+  400fe2:	39 f9                	cmp    %edi,%ecx      // rcx - edi
+  400fe4:	7e 0c                	jle    400ff2 <func4+0x24>  // jmp  to A if rcx <= edi
+  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx	  // edx = 6
+  400fe9:	e8 e0 ff ff ff       	callq  400fce <func4> // call func4(i, 0, 6)
+  400fee:	01 c0                	add    %eax,%eax    // return *= 2
+  400ff0:	eb 15                	jmp    401007 <func4+0x39>    // jmp to B (return) 
+
+  // A
+  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax   // return = 0
+  400ff7:	39 f9                	cmp    %edi,%ecx   // rcx - rdi
+  400ff9:	7d 0c                	jge    401007 <func4+0x39>  // jump to B (return)
+  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi  // esi = rcx + 0x1  (rcx = 8)
   400ffe:	e8 cb ff ff ff       	callq  400fce <func4>
   401003:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax
+
+  // B
   401007:	48 83 c4 08          	add    $0x8,%rsp
   40100b:	c3                   	retq   
 
-000000000040100c <phase_4>:
-  40100c:	48 83 ec 18          	sub    $0x18,%rsp
-  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
-  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
-  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi
-  40101f:	b8 00 00 00 00       	mov    $0x0,%eax
-  401024:	e8 c7 fb ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  401029:	83 f8 02             	cmp    $0x2,%eax
-  40102c:	75 07                	jne    401035 <phase_4+0x29>
-  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)
-  401033:	76 05                	jbe    40103a <phase_4+0x2e>
-  401035:	e8 00 04 00 00       	callq  40143a <explode_bomb>
-  40103a:	ba 0e 00 00 00       	mov    $0xe,%edx
-  40103f:	be 00 00 00 00       	mov    $0x0,%esi
-  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi
-  401048:	e8 81 ff ff ff       	callq  400fce <func4>
-  40104d:	85 c0                	test   %eax,%eax
-  40104f:	75 07                	jne    401058 <phase_4+0x4c>
-  401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp)
+000000000040100c <phase_4>: // rdi is input, i and j are first two ints
+  40100c:	48 83 ec 18          	sub    $0x18,%rsp	// add 3 bytes to rsp
+  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx	// arg4 = rsp + 12
+  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx   // arg3 = rsp + 8
+  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi	// %d $d
+  40101f:	b8 00 00 00 00       	mov    $0x0,%eax	// eax = 0
+  401024:	e8 c7 fb ff ff       	callq  400bf0 <__isoc99_sscanf@plt> // set first and second number you input to the stack pointer + 8 and + 12
+  401029:	83 f8 02             	cmp    $0x2,%eax	// check if theres 2 args
+  40102c:	75 07                	jne    401035 <phase_4+0x29> // jump to explode
+  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)	// i - 14
+  401033:	76 05                	jbe    40103a <phase_4+0x2e> // jmp if i <= 14
+  401035:	e8 00 04 00 00       	callq  40143a <explode_bomb> // explode
+  40103a:	ba 0e 00 00 00       	mov    $0xe,%edx  // arg3 = 14
+  40103f:	be 00 00 00 00       	mov    $0x0,%esi  // arg2 = 0
+  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi  // arg1 = i
+  401048:	e8 81 ff ff ff       	callq  400fce <func4>  // call func4(i, 0, 14)
+  40104d:	85 c0                	test   %eax,%eax    // check if ret is 0
+  40104f:	75 07                	jne    401058 <phase_4+0x4c> // explode
+  401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp)	// check if j = 0
   401056:	74 05                	je     40105d <phase_4+0x51>
   401058:	e8 dd 03 00 00       	callq  40143a <explode_bomb>
   40105d:	48 83 c4 18          	add    $0x18,%rsp
